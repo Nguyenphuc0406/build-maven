44cf80b7a04084b0c0277cc5129a40e15921c7e5
thuctap <thuctap@ViettelGroup.ViettelAD.com>
2021-01-21 11:33:13 +0700

    first update

---
+++ src/main/java/com/vds/api/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.vds.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+}
